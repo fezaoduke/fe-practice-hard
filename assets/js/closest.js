45693fc9a5c68fab659c119f9df822bbb1949513
wingmeng <wingmeng@qq.com>
2019-05-07 14:07:58 +0800

    :art: 调整结构

--- assets/js/closest.js
+++ assets/js/closest.js
@@ -1,21 +1,16 @@
-// closest polyfill
-window.Element && 'closest' in Element.prototype || +function() {
-  Element.prototype.closest = function(targetEl) {
-    var result = this.closestAll(targetEl);
-    return result.length === 0 ? null : result[0];
-  }
-}();
-
-Element.prototype.closestAll = function(targetEl) {
+/**
+ * 沿当前元素的 DOM 树向上遍历，找到所有匹配 `node` 选择器的父祖元素
+ */
+Element.prototype.closestAll = function(node) {
   var el = this;
   var nodelist;
 
   // 参数校验
-  if (typeof targetEl !== 'string' || !targetEl.trim()) {
-    throw Error('\' + targetEl + \' is not a valid selector');
+  if (typeof node !== 'string' || !node.trim()) {
+    throw Error('\' + node + \' is not a valid selector');
   }
 
-  nodelist = document.querySelectorAll(targetEl);
+  nodelist = document.querySelectorAll(node);
 
   // 使用 ES5 的 filter 过滤出包含 el 的元素
   return Array.prototype.slice.call(nodelist)
@@ -24,3 +19,13 @@ Element.prototype.closestAll = function(targetEl) {
     })
     .reverse();  // 反转数组，最近的排前面，依次从近到远
 }
+
+// closest polyfill（js 有原生的 closest 方法）
+window.Element && 'closest' in Element.prototype || +function() {
+  Element.prototype.closest = function(node) {
+    var result = this.closestAll(node);
+    return result.length === 0 ? null : result[0];
+  }
+}();
+
+
--- assets/js/multi-sort.js
+++ assets/js/multi-sort.js
@@ -1,3 +1,8 @@
+/**
+ * 对象数组多重排序
+ * @param {array} rules - 排序参照的字段名数组，注意顺序
+ * @param {boolean} [reversal] - 是否倒序，默认为 false
+ */
 function multiSort(rules, reversal) {
   return function(o, p) {
     var a, b, i;
